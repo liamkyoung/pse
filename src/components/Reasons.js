4b5d8f11d2d7988905c5546d92d80808bbce8134
Kingston <liamyoung@ufl.edu>
2021-10-21 15:17:52 -0400

    Added text under icons. Nothin much

--- src/components/Reasons.js
+++ src/components/Reasons.js
@@ -6,15 +6,18 @@ import Briefcase from '@heroicons/react/outline/BriefcaseIcon'
 function Reasons () {
   return (
     <div className='scrollable-section justify-center items-center thick-border-black'>
-      <div className='flex justify-between h-12 w-1/2'>
-        <div className='text-black hover:text-pse_yellow-dark w-12'>
+      <div className='flex justify-between h-12 w-1/2 text-2xl'>
+        <div className='text-center text-black hover:text-pse_yellow-dark w-12'>
           <StockIcon />
+          Skills
         </div>
-        <div className='text-black hover:text-pse_yellow-dark w-12'>
+        <div className='text-center text-black hover:text-pse_yellow-dark w-12'>
           <UserGroup />
+          Social
         </div>
-        <div className='text-black hover:text-pse_yellow-dark w-12'>
+        <div className='text-center text-black hover:text-pse_yellow-dark w-12'>
           <Briefcase />
+          Success
         </div>
       </div>
     </div>
